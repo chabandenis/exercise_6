756ae4fb286bf2f606384b6eda6fdef6b24da82a
Denis Chaban <denisbrd8@gmail.com>
2024-04-27 19:33:21 +0700

    ,

--- src/main/java/ru/chaban/exercise_4/service/RetailLoanChangeServiceImpl.java
+++ src/main/java/ru/chaban/exercise_4/service/RetailLoanChangeServiceImpl.java
@@ -5,8 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.chaban.exercise_4.domain.Account;
 import ru.chaban.exercise_4.domain.AccountPool;
+import ru.chaban.exercise_4.domain.TppProduct;
 import ru.chaban.exercise_4.repository.AccountPoolRepository;
 import ru.chaban.exercise_4.repository.AccountRepository;
+import ru.chaban.exercise_4.repository.TppProductRepository;
 import ru.chaban.exercise_4.request.CorporateSettlementAccountCreateRequestDto;
 import ru.chaban.exercise_4.request.CorporateSettlementInstanceCreateRequestDto;
 import ru.chaban.exercise_4.response.CorporateSettlementAccountCreateResponseDto;
@@ -26,9 +28,31 @@ public class RetailLoanChangeServiceImpl implements RetailLoanChangeService {
 
     private final AccountRepository accountRepository;
     private final AccountPoolRepository accountPoolRepository;
+    private final TppProductRepository productRepository;
 
     @Override
     public CorporateSettlementInstanceCreateResponseDto create(CorporateSettlementInstanceCreateRequestDto request) {
+/*        Шаг 1.5
+        Добавить в таблицу ПР (tpp_product_registry) строки, заполненные:
+•	Id - ключ таблицы
+•	product_id - ссылка на таблицу ЭП, где tpp_product.id  == tpp_product_registry.product_id
+•	type – тип ПР (лицевого/внутрибанковского счета)
+•	account_id – ид счета
+•	currency_code – код валюты счета
+•	state – статус счета, enum (0, Закрыт/1, Открыт/2, Зарезервирован/3, Удалён)
+        (см. задачу на создание продуктового регистра по методу corporate-settlement-account/create)
+*/
+
+        TppProduct tppProduct = new TppProduct();
+        tppProduct.setDays(55);
+        tppProduct.setNumber(request.getContractNumber());
+        tppProduct.setState("Open");
+        tppProduct.setRequisiteType(request.getRegisterType());
+        //...
+
+        productRepository.save(tppProduct);
+
+
         CorporateSettlementInstanceCreateResponseDto responseDto = new CorporateSettlementInstanceCreateResponseDto();
         DataCreate dat = new DataCreate();
         dat.setInstanceId("swef w we");
